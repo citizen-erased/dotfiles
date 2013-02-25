e71163c2f4af6c15671213bfc1d9a0e80d609bc1
citizen-erased <rasherb2@gmail.com>
2013-02-25 22:15:05 +0000

    added .xinitrc, .fehbg, mpd.service

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
+#!/bin/sh
+
+ln -s ~/dotfiles/.xinitrc ~/.xinitrc
+
 ln -s ~/dotfiles/gitconfig ~/gitconfig
 ln -s ~/dotfiles/gitignore ~/gitignore
 
@@ -8,6 +12,12 @@ ln -s ~/dotfiles/.vimrc ~/.vimrc
 
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 
+ln -s ~/dotfiles/.fehbg ~/.fehbg
+
 mkdir -p ~/.mpd/playlists
 touch ~/.mpd/{database,log,pid,state}
 ln -s ~/dotfiles/.mpdconf ~/.mpdconf
+mkdir -p ~/.config/systemd/user
+ln -s ~/dotfiles/.config/systemd/user/mpd.service ~/.config/systemd/user/mpd.service 
+echo "make sure the following line is in .xinitrc before exec <session>:"
+echo "systemd --user &"
